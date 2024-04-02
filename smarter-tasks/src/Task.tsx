1d4ef9b318bc65c0012d0782b2b9b0e5f1057265
Y-Dharvik <21951a0536@iare.ac.in>
2024-04-02 21:19:03 +0530

    Refactor TaskList, Task, and TaskApp components to use functional components

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import "./TaskCard.css";
 
 interface TaskProps {
@@ -7,15 +6,26 @@ interface TaskProps {
     dueDate: string;
 }
 
-class Task extends React.Component<TaskProps> {
-  render() {
-    return (
-      <div className="TaskItem shadow-md border border-slate-100">
-        <h3 className="text-base font-bold my-1">{this.props.title} ({this.props.dueDate})</h3>
-          {this.props.description}
-      </div>
-    );
-  }
+// class Task extends React.Component<TaskProps> {
+//   render() {
+//     return (
+//       <div className="TaskItem shadow-md border border-slate-100">
+//         <h3 className="text-base font-bold my-1">{this.props.title} ({this.props.dueDate})</h3>
+//           {this.props.description}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Task;
+
+const TaskFC = (props: TaskProps) => {
+  return (
+    <div className="TaskItem shadow-md border border-slate-100">
+      <h3 className="text-base font-bold my-1">{props.title} ({props.dueDate})</h3>
+        {props.description}
+    </div>
+  );
 }
 
-export default Task;
+export default TaskFC;
--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -9,61 +9,118 @@ interface TaskFormState {
   description: string;
   dueDate: string;
 }
-class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
-  constructor(props: TaskFormProps) {
-    super(props);
-    this.state = { title: "", description: "", dueDate: new Date().toDateString() };
+// class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
+//   constructor(props: TaskFormProps) {
+//     super(props);
+//     this.state = { title: "", description: "", dueDate: new Date().toDateString() };
+//   }
+//   addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+//     event.preventDefault();
+//     const newTask = {
+//       title: this.state.title,
+//       description: this.state.description,
+//       dueDate: this.state.dueDate,
+//     };
+//     this.props.addTask(newTask);
+//     this.setState({ title: "" });
+//     this.setState({ description: "" });
+//     this.setState({ dueDate: new Date().toDateString() });
+//   };
+
+
+//   dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+//     console.log(`${event.target.value}`);
+//     this.setState({ dueDate: event.target.value });
+//   };
+
+//   descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+//     console.log(`${event.target.value}`);
+//     this.setState({ description: event.target.value });
+//   };
+
+//   titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+//     console.log(`${event.target.value}`);
+//     this.setState({ title: event.target.value });
+//   };
+
+//   render(){
+//     return (
+//       <div className='font-bold text-xl text-red-500 text-center'>
+//         Task form
+//         <form onSubmit={this.addTask}>
+//           <input type="text" className = "border-2 border-gray-500" id="todoTitle" required
+//           placeholder="Title" value={this.state.title} onChange={this.titleChanged}
+//           />
+//           <input type="text" className = "border-2 border-gray-500" id="todoDescription" required
+//           placeholder="Description" value={this.state.description} onChange={this.descriptionChanged}
+//           />
+//           <input type="date" className = "border-2 border-gray-500" id="todoDueDate" required
+//           placeholder="Due Date" value={this.state.dueDate} onChange={this.dueDateChanged}
+//           />
+//           <button type="submit" id="addTaskButton"
+//           className="bg-blue-500 hover:bg-blue-700 text-white ml-2 font-bold py-1 px-3 rounded">
+//             Add item
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+//  export default TaskForm;
+
+ const TaskFormFC = (props: TaskFormProps) => {
+  const [formState, setFormState] = React.useState<TaskFormState>({
+    title: "",
+    description: "",
+    dueDate: "",
+  });
+
+  const titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(`${event.target.value}`);
+    setFormState({ ...formState, title: event.target.value });
   }
-  addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+
+  const descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(`${event.target.value}`);
+    setFormState({ ...formState, description: event.target.value });
+  }
+
+  const dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(`${event.target.value}`);
+    setFormState({ ...formState, dueDate: event.target.value });
+  }
+  
+  const addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    const newTask = {
-      title: this.state.title,
-      description: this.state.description,
-      dueDate: this.state.dueDate,
-    };
-    this.props.addTask(newTask);
-    this.setState({ title: "" });
-    this.setState({ description: "" });
-    this.setState({ dueDate: new Date().toDateString() });
+    console.log(`Submitted the form with`);
+    if (formState.title.length === 0 || formState.dueDate.length === 0) {
+      return;
+    }
+    props.addTask(formState);
+    setFormState({ title: "", description: "", dueDate: "" });
   };
 
+  return (
+    <div className='font-bold text-xl text-red-500 text-center'>
+      Task form
+      <form onSubmit={addTask}>
+        <input type="text" className = "border-2 border-gray-500" id="todoTitle" required
+        placeholder="Title" value={formState.title} onChange={titleChanged}
+        />
+        <input type="text" className = "border-2 border-gray-500" id="todoDescription" required
+        placeholder="Description" value={formState.description} onChange={descriptionChanged}
+        />
+        <input type="date" className = "border-2 border-gray-500" id="todoDueDate" required
+        placeholder="Due Date" value={formState.dueDate} onChange={dueDateChanged}
+        />
+        <button type="submit" id="addTaskButton"
+        className="bg-blue-500 hover:bg-blue-700 text-white ml-2 font-bold py-1 px-3 rounded">
+          Add item
+        </button>
+      </form>
+    </div>
+  );
+ };
 
-  dueDateChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    console.log(`${event.target.value}`);
-    this.setState({ dueDate: event.target.value });
-  };
-
-  descriptionChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    console.log(`${event.target.value}`);
-    this.setState({ description: event.target.value });
-  };
-
-  titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    console.log(`${event.target.value}`);
-    this.setState({ title: event.target.value });
-  };
-
-  render(){
-    return (
-      <div className='font-bold text-xl text-red-500 text-center'>
-        Task form
-        <form onSubmit={this.addTask}>
-          <input type="text" className = "border-2 border-gray-500" id="todoTitle" required
-          placeholder="Title" value={this.state.title} onChange={this.titleChanged}
-          />
-          <input type="text" className = "border-2 border-gray-500" id="todoDescription" required
-          placeholder="Description" value={this.state.description} onChange={this.descriptionChanged}
-          />
-          <input type="date" className = "border-2 border-gray-500" id="todoDueDate" required
-          placeholder="Due Date" value={this.state.dueDate} onChange={this.dueDateChanged}
-          />
-          <button type="submit" id="addTaskButton"
-          className="bg-blue-500 hover:bg-blue-700 text-white ml-2 font-bold py-1 px-3 rounded">
-            Add item
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
- export default TaskForm;
+ export default TaskFormFC;
+ 
