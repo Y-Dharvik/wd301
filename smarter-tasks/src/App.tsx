fd71103605fb0283fe421d111a27578b862afc91
Y-Dharvik <21951a0536@iare.ac.in>
2024-04-26 20:21:32 +0530

    Designed the application state using Context and useReducer

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -2,14 +2,16 @@ import React, { useContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes"
 import { ThemeContext } from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
 
 const App = () => {
   const { theme } = useContext(ThemeContext)
   return (
     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-      {theme}
+    <ProjectsProvider>
       <RouterProvider router={router} />
-    </div>
+    </ProjectsProvider>
+  </div>
   );
 }
 export default App;
