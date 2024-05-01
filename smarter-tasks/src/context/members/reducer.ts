c60d8a7988d36e9131bfb95b5e3f2ef880a09386
Y-Dharvik <21951a0536@iare.ac.in>
2024-05-01 21:32:31 +0530

    Added tasks to the project

--- smarter-tasks/src/context/members/reducer.ts
+++ smarter-tasks/src/context/members/reducer.ts
@@ -1,4 +1,5 @@
 interface Member {
+    id: Key | null | undefined;
     email: string;
     name: string;
     password: string;
--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 import AccountLayout from "../layouts/account"
 import ProtectedRoute from "./ProtectedRoutes"
 import Signin from "../pages/signin"
@@ -8,6 +8,10 @@ import Projects from "../pages/projects"
 import Members from "../pages/members"
 import Logout from "../pages/logout";
 import Notfound from "../pages/Notfound";
+import ProjectContainer from "../pages/projects/ProjectContainer";
+import ProjectDetails from "../pages/project_details";
+import NewTask from "../pages/tasks/NewTask";
+import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
 
 const router = createBrowserRouter([
   {
@@ -45,7 +49,33 @@ const router = createBrowserRouter([
       },
       {
         path: "projects",
-        element: (<Projects />)
+        element: <ProjectContainer />,
+        children: [
+          { index: true, element: <Projects /> },
+          {
+            path: ":projectID",
+            element: <ProjectDetails />,
+            children: [
+              { index: true, element: <></> },
+              {
+                path: "tasks",
+                children: [
+                  { index: true, element: <Navigate to="../" /> },
+                  {
+                    path: "new",
+                    element: <NewTask />,
+                  },
+                  {
+                    path: ":taskID",
+                    children: [
+                      { index: true, element: <TaskDetailsContainer /> },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       },
       {
         path: "members",
